68e8bba910169ec706bb6dbda5b35c0c223c1667
Mefa <methbestuser@gmail.com>
2026-08-25 10:04:21 +0700

    worldgen

--- media/scripts/generation_data.gd
+++ media/scripts/generation_data.gd
@@ -3,13 +3,9 @@ class_name GenerationData
 
 @export var name : StringName
 
-@export_group("Noise settings")
-@export_range(0,99999,1.0) var seed : int
-@export var base_height : int
-@export var height_amplitude: float
-@export var noise_frequency: float
-@export var noise_octaves: int
+@export var noise : NoiseTexture3D
 
-@export_group("Levels")
-@export var sea_level: int
-@export var dirt_layer_thickness : int
+@export_group("Blocks")
+@export var layers : Array[Dictionary]
+
+@export var match_script: String = ""
--- media/scripts/world_generator.gd
+++ media/scripts/world_generator.gd
@@ -1,8 +1,15 @@
 extends RefCounted
 class_name WorldGenerator
 
-var data : GenerationData
-var noise : FastNoiseLite
+var raw_data : GenerationData:
+	set(v):
+		if raw_data != null:
+			if raw_data.match_script != "":
+				var dynamic_script = GDScript.new()
+				dynamic_script.source_code = "extends Resource\n\nfunc match_block(x:int,y:int,z:int):\n\t"
+				dynamic_script.reload()
+				runtime_data.set_script(dynamic_script)
+var runtime_data : Resource
 
 func _init() -> void:
-	noise = FastNoiseLite.new()
+	pass
